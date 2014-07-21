174567e374b367723ba5001e612e9879586d422e
Suresh <suresh@como-HP-dx2080-MT-KS826PA.(none)>
2014-07-21 11:33:34 +0530

    Symfony2 set up and Initial items add using angular

---
+++ src/Project/CommonBundle/Controller/ItemController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Project\CommonBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\ORM\EntityManager;
+
+use Project\CommonBundle\Entity\Item;
+use Project\CommonBundle\Entity\ItemRepository;
+class ItemController extends Controller
+{
+    public function itemsAction()
+    {
+        return $this->render('ProjectCommonBundle:Item:items.html.twig');
+    }
+
+    public function itemsApiAction()
+    {
+        /** @var  $em EntityManager */
+        $em = $this->getDoctrine()->getManager();
+
+        /** @var  $itemRepo ItemRepository */
+        $itemRepo = $em->getRepository('ProjectCommonBundle:Item');
+        $items = $itemRepo->findAllData();
+
+        $response = new Response(json_encode($items));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+
+    public function addItemApiAction(Request $request){
+
+        $data = json_decode($request->getContent(), true);
+        $itemName = $data['name'];
+        $itemPrice = $data['price'];
+
+        $item = new Item();
+        $item->setName($itemName);
+        $item->setPrice($itemPrice);
+
+        /** @var  $em EntityManager */
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($item);
+        $em->flush();
+
+        $em->refresh($item);
+
+        /** @var  $itemRepo ItemRepository */
+        $itemRepo = $em->getRepository('ProjectCommonBundle:Item');
+        $addedItem = $itemRepo->findData($item->getId());
+
+        $response = new Response(json_encode($addedItem,1));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+}
---
+++ src/Project/CommonBundle/Entity/Item.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Project\CommonBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Item
+ *
+ * @ORM\Table(name="items")
+ * @ORM\Entity(repositoryClass="Project\CommonBundle\Entity\ItemRepository")
+ */
+class Item
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="price", type="string", length=255)
+     */
+    private $price;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Item
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set price
+     *
+     * @param string $price
+     * @return Item
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return string 
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
