d4e3415a210092772a521182be95ef2d75c1f299
Suresh <suresh@como-HP-dx2080-MT-KS826PA.(none)>
2014-07-22 09:22:15 +0530

    Moved js code into external file

---
+++ src/Project/CommonBundle/Resources/public/js/project/item.js
@@ -0,0 +1,59 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: suresh
+ * Date: 22/07/14
+ * Time: 08:53
+ * To change this template use File | Settings | File Templates.
+ */
+
+var module = angular.module("customAPP",[]);
+angular.element(document).ready(function(){
+    angular.bootstrap(document.getElementById('custom-id'),['customAPP']);
+});
+
+module.config(function($interpolateProvider){
+    $interpolateProvider.startSymbol('%');
+    $interpolateProvider.endSymbol('%');
+
+});
+
+module.controller('ItemController',function($scope,$http){
+
+    // Pagination
+    $scope.currentPage = 0;
+    $scope.pageSize = 10;
+    $scope.numberOfPages=function(){
+        return Math.ceil((($scope.items)?$scope.items.length:0)/$scope.pageSize);
+    }
+
+    // Loading all items while loading page
+    $http({method:'GET',url: '/api/items'}).success(function(data){
+        $scope.items = data;
+    });
+
+    // Adding item
+    $scope.addItem = function(){
+        var formData = { name: $scope.itemName , price: $scope.price };
+        $http({
+            method: 'POST',
+            url: '/api/post/item',
+            data: formData }).
+            success(function(data, status, headers, config) {
+                angular.forEach(data, function(value){
+                    $scope.items.push(value);
+                });
+            }).
+            error(function(data, status, headers, config) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
+    }
+});
+
+module.filter('startFrom', function() {
+    return function(input,start) {
+        if(input != undefined){
+            return input.slice(start);
+        }
+    }
+});
